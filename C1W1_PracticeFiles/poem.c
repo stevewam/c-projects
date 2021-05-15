9b1caff5337ed7ff19ec580330ca7db2954701f7
Steven Williams <stevenwa.lee@gmail.com>
2021-05-16 00:58:14 +0800

    First commit

---
+++ C1W1_PracticeFiles/poem.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int main(void)
+{
+    printf("London by William Blake\n"
+           "By William Blake\n\n"
+
+           "I wander thro' each charter' d street,\n"
+           "Near where the charter'd Thames does flow.\n"  
+           "And mark in every face I meet\n"
+           "Marks of weakness,marks of woe.\n\n"
+
+           "In every cry of every Man,\n"
+           "In every Infants cry of fear,\n"
+           "In every voice: in every ban,\n"
+           "The mind - forg'd manacles I hear\n\n"
+
+           "How the Chimney - sweepers cry\n"
+           "Every blackning Church appalls,\n"
+           "And the hapless Soldiers sigh\n"
+           "Runs in blood down Palace walls\n\n"
+
+           "But most thro' midnight streets I hear\n"
+           "How the youthful Harlots curse\n"
+           "Blasts the new - born Infants tear\n"
+           "And blights with plagues the Marriage hearse\n\n\n");
+    return 0;
+}    
+
