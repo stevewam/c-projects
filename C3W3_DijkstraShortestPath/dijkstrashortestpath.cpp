fa812d96a0aed8ff31b8530c4b429f4a6581c402
Steven Williams <stevenwa.lee@gmail.com>
2021-05-20 01:06:19 +0800

    "Completed Graph class for Dijkstra"

--- C3W3_DijkstraShortestPath/dijkstrashortestpath.cpp
+++ C3W3_DijkstraShortestPath/dijkstrashortestpath.cpp
@@ -2,29 +2,31 @@
 #include <cstdlib>
 #include <ctime>
 #include <vector>
+#include <iomanip>
+#include <exception>
 using namespace std;
 
-const double MIN_DIST = 1.0;
-const double MAX_DIST = 10.0;
+const double MIN_DIST = 10.0;
+const double MAX_DIST = 100.0;
+const double RANGE = MAX_DIST - MIN_DIST;
 
 double prob() {
-    return (rand() % 100)/100;
+    return double(rand())/RAND_MAX;
 }
 
 class Graph {
 public:
-    // Graph(): n(0), adj(0) {}
     Graph(int n, double density);
     int V();
-    // void E(vector <pair<int, double> > adj[]);
-    void adjacent(vector <pair<int, double> > adj[], int u, int v);
-    // void neighbors(vector <pair<int, double> > adj[], int u);
+    int E();
+    bool adjacent(int u, int v);
+    vector<int> neighbors(int u);
     void add(int u, int v, double dist);
-    // void remove(vector <pair<int, double> > adj[], int u, int v, double dist);
-    // void get_node_value(vector <pair<int, double> > adj[], int u);
-    // void set_node_value(vector <pair<int, double> > adj[], int u);
-    // void get_edge_value(vector <pair<int, double> > adj[], int u, int v);
-    // void set_edge_value(vector <pair<int, double> > adj[], int u, int v, double dist);
+    void remove(int u, int v);
+    // void get_node_value(int u);
+    // void set_node_value(int u, double a);
+    double get_edge_value(int u, int v);
+    void set_edge_value(int u, int v, double dist);
     void print();
 private:
     int n;
@@ -35,22 +37,33 @@ Graph::Graph(int n, double density){
     srand(time(0));
     this->n = n;
     adj = new vector <pair<int, double> > [n];
+    int count = 0;
 
     for (int i = 0; i < n; i++){
         for (int j = 0; j < n; j++){
             if (i == j) 
                 continue;
             else
-                if (prob() < density) {
-                    double dist = MIN_DIST + (rand() % int(MAX_DIST));
+                if (prob() < density && !Graph::adjacent(i, j) && !Graph::adjacent(j, i)) {
+                    double dist = (MIN_DIST + (rand() % int(RANGE)))/10.0;
                     Graph::add(i, j, dist);
+                    count++;
                 }           
         }
     }
+    cout << count << "\n";
 };
 
 int Graph::V(){
     return n;
+}
+
+int Graph::E(){
+    int sum = 0;
+    for (int i = 0; i < n; i++){
+        sum += adj[i].size();
+    }
+    return sum;
 }
 
 void Graph::add( int u, int v, double dist){
@@ -58,26 +71,100 @@ void Graph::add( int u, int v, double dist){
     adj[v].push_back(make_pair(u, dist));
 };
 
+void Graph::remove(int u, int v){
+    int w;
+    int nodes[2] = {u, v};
+    int flag = 1;
+
+    for (int i = 0; i < 2; i++){
+        int j = !i;
+        if (flag == 0) break;
+        for (auto it=adj[nodes[i]].begin(); it!=adj[nodes[i]].end(); it++){
+            w = it->first;
+            if (w == nodes[j]) {
+                adj[nodes[i]].erase(it);
+                flag = 0;
+                break;
+            }   
+        }
+    }
+
+}
+
+bool Graph::adjacent(int u, int v){
+    int w;
+    
+    for (auto it=adj[u].begin(); it!=adj[u].end(); it++){
+        w = it->first;
+        if (w == v) return 1;
+    }
+    return 0;
+}
+
+vector<int> Graph::neighbors(int u){
+    vector<int> neighbors_nodes;
+    int w;
+
+    for (auto it=adj[u].begin(); it!=adj[u].end(); it++){
+        w = it->first;
+        neighbors_nodes.push_back(w);
+    }
+    return neighbors_nodes;
+}
+
 void Graph::print(){
-    int v, w;
-    for (int u = 0; u < n; u++)
-    {
-        cout << "Node " << u << " makes an edge with \n";
-        for (auto it=adj[u].begin(); it!=adj[u].end(); it++)
-        {
-            v = it->first;
-            w = it->second;
-            cout << "\tNode " << v << " with edge weight ="
-                 << w << "\n";
+    double u, v;
+    // for (int i = 0; i < n; i++){
+    for (int i = 0; i < n; i++){
+        cout << "Node " << i << "\n";
+        for (auto it=adj[i].begin(); it!=adj[i].end(); it++){
+            cout << "\t-> ";
+            u = it->first;
+            v = it->second;
+            cout << "[" << u << ", dist=";
+            cout << setprecision(2) << v << "]\n";
         }
         cout << "\n";
     }
 }
 
+double Graph::get_edge_value(int u, int v) {
+    int w;
+    double y; 
+    
+    for (auto it=adj[u].begin(); it!=adj[u].end(); it++){
+        w = it->first;
+        y = it->second;
+        if (w == v) return y;
+    }
+    return -1.0;
+}
+
+void Graph::set_edge_value(int u, int v, double dist) {
+    int w;
+    double y; 
+    
+    for (auto it=adj[u].begin(); it!=adj[u].end(); it++){
+        w = it->first;
+        if (w == v) {
+            it->second = dist;
+            break;
+        }
+    }
+}
+
 int main(void) {
-    Graph test(50, 0.4);
+    Graph test(50, 0.1);
     test.print();
-    cout << "Done" << static_cast<int>(test.V());
+    // vector<int> neigh = test.neighbors(0);
+    // for (auto it=neigh.begin(); it!=neigh.end(); it++){
+    //         cout << *it << ", ";
+    //     }
+    // cout << test.set_edge_value(0, 1, 90) << "\n";
+    // test.set_edge_value(0, 1, 9.0) ;
+    
+    // test.print();
+
     
     return 0;
 }
