b859a31047c947a6d91bdd84f30f7d8092dd37d5
Steven Williams <stevenwa.lee@gmail.com>
2021-05-19 23:08:56 +0800

    "Modified Dijkstra"

--- C3W3_DijkstraShortestPath/dijkstrashortestpath.cpp
+++ C3W3_DijkstraShortestPath/dijkstrashortestpath.cpp
@@ -13,49 +13,72 @@ double prob() {
 
 class Graph {
 public:
-    Graph(): adj(0), density(0) {}
+    // Graph(): n(0), adj(0) {}
     Graph(int n, double density);
-    void V(vector <pair<int, double>> adj[]);
-    void E(vector <pair<int, double>> adj[]);
-    void adjacent(vector <pair<int, double>> adj[], int u, int v);
-    void neighbors(vector <pair<int, double>> adj[], int u);
-    void add(vector <pair<int, double>> adj[], int u, int v, double dist);
-    void remove(vector <pair<int, double>> adj[], int u, int v, double dist);
-    void get_node_value(vector <pair<int, double>> adj[], int u);
-    void set_node_value(vector <pair<int, double>> adj[], int u);
-    void get_edge_value(vector <pair<int, double>> adj[], int u, int v);
-    void set_edge_value(vector <pair<int, double>> adj[], int u, int v, double dist);
+    int V();
+    // void E(vector <pair<int, double> > adj[]);
+    void adjacent(vector <pair<int, double> > adj[], int u, int v);
+    // void neighbors(vector <pair<int, double> > adj[], int u);
+    void add(int u, int v, double dist);
+    // void remove(vector <pair<int, double> > adj[], int u, int v, double dist);
+    // void get_node_value(vector <pair<int, double> > adj[], int u);
+    // void set_node_value(vector <pair<int, double> > adj[], int u);
+    // void get_edge_value(vector <pair<int, double> > adj[], int u, int v);
+    // void set_edge_value(vector <pair<int, double> > adj[], int u, int v, double dist);
     void print();
 private:
-    vector <pair<int, double>>* adj;
-    double density;
+    int n;
+    vector <pair<int, double> >* adj;
 };
 
 Graph::Graph(int n, double density){
     srand(time(0));
-    vector <pair<int, double>> adj[n];
+    this->n = n;
+    adj = new vector <pair<int, double> > [n];
 
     for (int i = 0; i < n; i++){
-        for (int j = 0; j < n; i++){
+        for (int j = 0; j < n; j++){
             if (i == j) 
                 continue;
             else
                 if (prob() < density) {
                     double dist = MIN_DIST + (rand() % int(MAX_DIST));
-                    Graph::add(adj, i, j, dist);
+                    Graph::add(i, j, dist);
                 }           
         }
     }
 };
 
-void Graph::add(vector <pair<int, double>> adj[], int u, int v, double wt){
-    adj[u].push_back(make_pair(v, wt));
-    adj[v].push_back(make_pair(u, wt));
+int Graph::V(){
+    return n;
+}
+
+void Graph::add( int u, int v, double dist){
+    adj[u].push_back(make_pair(v, dist));
+    adj[v].push_back(make_pair(u, dist));
 };
 
+void Graph::print(){
+    int v, w;
+    for (int u = 0; u < n; u++)
+    {
+        cout << "Node " << u << " makes an edge with \n";
+        for (auto it=adj[u].begin(); it!=adj[u].end(); it++)
+        {
+            v = it->first;
+            w = it->second;
+            cout << "\tNode " << v << " with edge weight ="
+                 << w << "\n";
+        }
+        cout << "\n";
+    }
+}
+
 int main(void) {
-    // Graph test(50, 0.4);
-    cout << "Done";
+    Graph test(50, 0.4);
+    test.print();
+    cout << "Done" << static_cast<int>(test.V());
+    
     return 0;
 }
 
@@ -63,19 +86,4 @@ int main(void) {
 
 
 
-// void printGraph(vector<pair<int,int> > adj[], int V)
-// {
-//     int v, w;
-//     for (int u = 0; u < V; u++)
-//     {
-//         cout << "Node " << u << " makes an edge with \n";
-//         for (auto it = adj[u].begin(); it!=adj[u].end(); it++)
-//         {
-//             v = it->first;
-//             w = it->second;
-//             cout << "\tNode " << v << " with edge weight ="
-//                  << w << "\n";
-//         }
-//         cout << "\n";
-//     }
-// }
+
